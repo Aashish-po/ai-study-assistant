f38b406844a4b928a1937b2e8452b2748e0c8b6f
Aashsish <poudelashish572@gmail.com>
2026-02-22 22:37:37 +0545

    commit

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,10 @@
-/* eslint-disable no-undef */
 // metro.config.js
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
-
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
   forceWriteFileSystem: true,
 });
