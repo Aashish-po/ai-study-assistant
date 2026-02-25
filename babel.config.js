f81aaf34fcbea283e012e267343a2071f46d88e3
Aashsish <poudelashish572@gmail.com>
2026-02-25 19:59:16 +0545

    Check for a "Duplicate Plugin" Error

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // Ensure you don't have conflicting plugins here
+    plugins: [], // Only add plugins if you absolutely need them!
   };
 };
